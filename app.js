7cb658e2f94359c41d912aba8ec4adba388fdba7
mannyuiux <mannyuiux@gmail.com>
2019-10-28 20:52:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import Main from './components/Main.vue';
+new Vue({
+    render: (createEl) => createEl(Main)
+}).$mount('#app');
---
+++ components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello from Vue and webpack.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main"
+};
+</script>
+
+<style>
+body {
+  font-family: "Roboto", sans-serif;
+}
+h1 {
+  text-align: center;
+}
+</style>
